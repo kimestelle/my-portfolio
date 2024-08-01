a2c431a1148876492b510b4e3341e1fdb703eea6
kimestelle <137699667+kimestelle@users.noreply.github.com>
2024-07-31 21:22:13 -0700

    adjusted name size

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <div className='flex flex-col w-full h-screen justify-center items-start pl-10 sm:pl-20'>
-            <h1 className="-mt-[30svh] text-[5em] md:text-[8em]">Estelle Kim</h1>
+            <h1 className="-mt-[30svh] text-[4em] md:text-[8em]">Estelle Kim</h1>
             <div className='flex flex-col sm:flex-row text-[0.8em] md:text-[1.3em] gap-1 mt-[1em]'>
                 <h3 className='w-max -mb-[0.5em]'>
                     Digital Media Design & Computer Science
