ec424976b6ea25f44c74e23219b518d21bfa6c2e
kimestelle <137699667+kimestelle@users.noreply.github.com>
2024-12-28 12:45:02 -0800

    fixed coursework section design

--- src/app/components/blocks/CourseBlock.tsx
+++ src/app/components/blocks/CourseBlock.tsx
@@ -19,13 +19,13 @@ export default function CourseBlock({ course }: CourseProps) {
   return (
     <div onClick={handleClick} className='w-full h-16 md:h-15 clickable'>
         <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-            <div key="front" className='h-16 md:h-15 flex sm:flex-row flex-col p-2 border-b border-gray-100 items-start justify-center sm:items-center sm:justify-start gap-2'>
-            <h5 className='leading-tight max-md:text-sm font-normal'>&gt; {course.number} <span className='text-black'>{course.name}</span></h5>
-            <ol className='list-none flex-row gap-1 items-center sm:pt-2 pb-0.5 hidden sm:flex'>
-                {course.languages.map((skill, index) => (
-                    <li key={index} className='bg-gray-200 p-0.5 px-1.5 mb-1 text-sm rounded-lg'>{skill}</li>
-                ))}
-            </ol>
+            <div key="front" className='h-16 md:h-15 flex flex-col p-2 border-b border-gray-100 items-start gap-1'>
+              <h5 className='leading-tight max-md:text-sm font-normal'>&gt; {course.number} <span className='text-black'>{course.name}</span></h5>
+              <ol className='list-none  pl-2 flex flex-row gap-1 items-center pb-0.5'>
+                  {course.languages.map((skill, index) => (
+                      <li key={index} className='shadow-inner p-0.5 px-1.5 text-sm rounded-lg'>{skill}</li>
+                  ))}
+              </ol>
             </div>
 
             <div key="back" className='w-full h-16 md:h-15 flex flex-col p-1.5 text-sm sm:text-base'>
